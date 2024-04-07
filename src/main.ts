db57bac3174971b3582484d16d3905910df11aae
CrauxZahard <vikisstar418@gmail.com>
2024-04-07 15:40:57 +0700

    feat: added main.ts

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { Application } from 'pixi.js';
+import './css/main.css';
+import { RouteManager } from './manager/route_manager';
+
+export const app = new Application();
+
+await app.init({ background: "#000000", resizeTo: window, preference: "webgl"});
+
+document.body.appendChild(app.canvas);
+
+export const router = new RouteManager();
+
+router.startGame();
