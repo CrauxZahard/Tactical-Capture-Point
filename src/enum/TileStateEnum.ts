05ba65bc6bf0ea52cfe1410bdd04214d209d6036
CrauxZahard <vikisstar418@gmail.com>
2024-04-09 00:53:04 +0700

    feat: new enum TileState

---
+++ src/enum/TileStateEnum.ts
@@ -0,0 +1,5 @@
+export enum TileState {
+    BLANK, // no character in this state, can move here
+    RESERVED, // character/objects is standing here, can't move here
+    NO_TILE // cant move here, there are no tiles.
+}
