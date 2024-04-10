a60d163e3e6882d7da8190a4ea7723f69bfd305a
TezadaConnect <terrencecalzada@gmail.com>
2024-04-10 15:50:34 +0800

    wip: Added tilemap and drag camera

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { RouteManager } from './manager/route_manager';
 
 export const app = new Application();
 
+globalThis.__PIXI_APP__ = app;
+
 await app.init({ background: "#000000", resizeTo: window, preference: "webgl"});
 
 document.body.appendChild(app.canvas);
--- src/views/main_scene_view.ts
+++ src/views/main_scene_view.ts
@@ -1,11 +1,13 @@
-import { Assets, Texture } from "pixi.js";
+import { Assets, FederatedEvent, FederatedPointerEvent, Texture } from "pixi.js";
 import { PixiView } from "../core/PixiView";
 import { Tile } from "../custom_pixis/Tile";
+// import {  } from "";
 import { PlayerModel } from "../models/PlayerModel";
 import { Character } from "../custom_pixis/Character";
 import { CharacterRoleEnum } from "../enums/CharacterRoleEnum";
 import { TurnManager } from "../manager/turn_manager";
 import { router } from "../main";
+import { CompositeTilemap, Tilemap } from "@pixi/tilemap";
 
 const lifter = new Character({
     name: "Droid-01",
@@ -37,30 +39,67 @@ player_2.addCharacter(lifter, support, sprinter)
 
 const turn = new TurnManager(player_1, player_2)
 export class MainSceneView extends PixiView {
+
+    constructor(){
+        super("#000000");
+    }
  
     private _image: Tile | null = null
     private _tile: Texture | null = null;
+    private _tilemap: CompositeTilemap | null = null;
+    private _isClicking: boolean = false;
+    private _eventData: FederatedPointerEvent | null = null;
 
     public override async ready(): Promise<void> {
-        this._tile = await Assets.load(router.getAssetImage("tiles/red-tile.png"));
+        this._tile = await Assets.load(router.getAssetImage("tiles/normal-tile.png"));
         console.log("Initializing");
-        this.addImage();
+        this.drawTilemap();
+        this.setCameraMovement();
         test()
     }   
 
     public override process(): void {
-        if(this._image){
-            // this._image.rotation += 0.02;
+        if(this._tilemap!.scale.x > 0.3){
+            this._tilemap!.scale.x -= 0.01;
+            this._tilemap!.scale.y -= 0.01;
         }
     }
 
-    public addImage(){
-        this._image = new Tile(this._tile!, {x: 0, y:0});
-        this._image.x = this.width / 2;
-        this._image.y = this.height / 2;
-        this._image.scale = 0.5;
-        this._image.anchor.set(0.5);
-        this.addChild(this._image);
+    private setCameraMovement(){
+        if(this._tilemap == null){
+            return;
+        }
+        this.interactive = true;
+        this.on("pointerdown", (event) => {
+            this._eventData = event;
+            this._isClicking = true;
+        });
+        this.on("pointerup", () => {
+            this._eventData = null;
+            this._isClicking = false;
+        });
+        this.on('pointermove', () => {
+            if (this._isClicking) {
+                const newPosition = this._eventData!.getLocalPosition(this._tilemap!.parent);
+                this._tilemap!.x = newPosition.x * 0.5;
+                this._tilemap!.y = newPosition.y * 0.5;
+            }
+        });
+    }
+
+    private drawTilemap(){
+        this._tilemap = new CompositeTilemap();
+
+        this._tilemap.position.x = 30;
+        this._tilemap.position.y = 30;
+
+        for (let i = 0; i < 15; i++) {
+            for (let j = 0; j < 15; j++) { 
+                this._tilemap.tile(this._tile!, i * 200 , j * 200);
+            }
+        }
+
+        this.addChild(this._tilemap);
     }
 
 }
